577917786f73990f68b4bebecd3e8f7b65e6a6dd
huangxiatong02 <1059838187@qq.com>
2023-04-09 23:42:00 +0800

    update 4.9

--- R/dong_cell_anno.R
+++ R/dong_cell_anno.R
@@ -4,23 +4,25 @@
 
 
 
-#' Title
+#' single cell annotation by scPred
 #'
-#' @param sce
-#' @param sce_ref
-#' @param name
-#' @param mini_cluster
-#' @param threshold
-#' @param merge_seurat_cluster
-#' @param name_merge_seurat
-#' @param source
-#' @param path_ref
+#'  Cell type annotation using a combination of unbiased feature selection from a reduced-dimension space, and machine-learning probability-based prediction method.
+#'  Refer to [scPred](https://genomebiology.biomedcentral.com/articles/10.1186/s13059-019-1862-5)
+#' @param sce A seurat object containing cells to be classified
+#' @param sce_ref A Seurat object with trained model(s) using scPred or a scPred object
+#' @param name A character used to name a new column of metadata containing cell type annotation by `scPred`
+#' @param mini_cluster Minimal cell counts of the cluster
+#' @param threshold Threshold used for probabilities to classify cells into classes. All cells below this threshold value will be labels as "unassigned". In the case of binary classification (two cell tyoes), a threshold of 0.5 will force all cells to be classified to any of the two cell types. For multi-class classification, if there's no probability higher than the threshold associated to a cell type, this will be labelled as "unassigned"
+#' @param merge_seurat_cluster Whether to merge prediction results by`scPred` with clustering results
+#' @param name_merge_seurat A character used to name a new column of metadata containing the results of `merge_seurat_cluster`
+#' @param source Character string related to computer system name choose from win and linux
+#' @param path_ref The path or the name of the file where the `sce_ref` is read from
 #'
-#' @return
+#' @return Seurat object with updated metadata containing cell type annotation by `scPred`
 #' @export
 #'
 #' @examples
-dong_cell_anno<-function(sce, sce_ref = NULL, name = "Model1", merge_seurat_cluster = T, name_merge_seurat = "scpred_seurat", mini_cluster = 50, threshold = 0.65, source = "win", path_ref = NULL){
+dong_cell_anno<-function(sce, sce_ref = NULL, name = "Model1",merge_seurat_cluster = T, name_merge_seurat = "scpred_seurat", mini_cluster = 50, threshold = 0.65, source = "win", path_ref = NULL){
 
 
   if(is.null(sce_ref)){
